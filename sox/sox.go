f0b48b2ecd4dbe92124550d2fdc19bc2629e4614
lzt <2564272712@qq.com>
2018-05-06 23:06:30 +0800

    str

--- sox/sox.go
+++ sox/sox.go
@@ -8,10 +8,10 @@ import (
 func init() {
 	cmd := exec.Command("which", "play")
 	if err := cmd.Run(); err != nil {
-		fmt.Println("没有找到play命令，发音将无法使用")
-		fmt.Println("ubuntu: sudo apt-get install sox")
-		fmt.Println("centos: sudo yum install sox")
-		fmt.Println()
+		str := "没有找到play命令，发音将无法使用\n"
+		str += "ubuntu: sudo apt-get install sox\n"
+		str += "centos: sudo yum install sox\n"
+		fmt.Println(str)
 	}
 }
 
