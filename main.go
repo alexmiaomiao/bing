e27fbcc7b9aad16f1f0c12372e77bfc37e26989d
lzt <2564272712@qq.com>
2018-05-08 23:21:43 +0800

    templ

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 	"log"
 	"flag"
+	"os"
 	"github.com/bing/jsonapi"
 	"github.com/bing/sox"
+	"github.com/bing/templ"
 )
 
 var verbose = flag.Bool("v", false, "查看例句")
@@ -15,39 +15,20 @@ var bre = flag.Bool("b", false, "美式发音")
 
 var complete chan int = make(chan int)
 
-func printres(res *jsonapi.SearchResult) {
-	if res.Word != "" {
-		fmt.Printf("%s\t", res.Word)
-	} else {
-		log.Fatal("no such word")
-	}
-
-	if res.Pronunciation != nil {
-		fmt.Printf("[美 %s]\t[英 %s]", res.Pronunciation.AmE, res.Pronunciation.BrE)
-	}
-
-	fmt.Println()
-	fmt.Println("---------------------------------------------------------------------------")
+func bingText(res *jsonapi.SearchResult) {
+	s := &struct {
+		Res *jsonapi.SearchResult
+		Verbose bool
+	}{res, *verbose}
 	
-	for _, item := range res.Defs {
-		fmt.Println(item.Pos, "\t", item.Def)
+	if err := templ.CommandLine.Execute(os.Stdout, s); err != nil {
+		log.Println(err)
 	}
 	
-	fmt.Println("---------------------------------------------------------------------------")
-	
-	if *verbose {
-		for _, item := range res.Sams {
-			fmt.Println(item.Eng)
-			fmt.Println(item.Chn)
-			fmt.Println()
-		}
-	} else {
-		fmt.Printf("使用 bing -v %s 可以查看例句\n", strings.Join(flag.Args(), " "))
-	}
 	complete <- 0
 }
 
-func pronres(res *jsonapi.SearchResult) {
+func bingSound(res *jsonapi.SearchResult) {
 	if res.Pronunciation != nil {
 		if *ame {
 			sox.Play(res.Pronunciation.AmEMP3)
@@ -56,18 +37,24 @@ func pronres(res *jsonapi.SearchResult) {
 			sox.Play(res.Pronunciation.BrEMP3)
 		}
 	}
+	
 	complete <- 0
 }
 
 func main() {
 	flag.Parse()
 	result, err := jsonapi.Search(flag.Args())
+	
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if "" == result.Word {
+		log.Fatal("no such word")
+	}
 	
-	go printres(result)
-	go pronres(result)
+	go bingText(result)
+	go bingSound(result)
 	
 	<- complete
 	<- complete
---
+++ templ/templ.go
@@ -0,0 +1,19 @@
+package templ
+
+import (
+	"text/template"
+)
+
+const t1 =
+`{{.Res.Word}}{{if .Res.Pronunciation}}    [美 {{.Res.Pronunciation.AmE}}]    [英 {{.Res.Pronunciation.BrE}}]{{end}}
+----------------------------------------------------------------------------------------------------
+{{range .Res.Defs}}{{.Pos}}    {{.Def}}
+{{end}}----------------------------------------------------------------------------------------------------
+{{if .Verbose}}{{range .Res.Sams}}{{.Eng}}
+{{.Chn}}
+
+{{end}}{{else}}使用 bing -v {{.Res.Word}} 可以查看例句
+{{end}}
+`
+
+var CommandLine = template.Must(template.New("commandline").Parse(t1))
